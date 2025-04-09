9093753e88603c3a31198c12826132d772a2283b
vedant <vedantboddula9860gmail.com>
2025-04-10 01:16:05 +0530

    sdas

--- port.js
+++ port.js
@@ -11,3 +11,29 @@ document.querySelectorAll('nav a').forEach(anchor => {
         });
     });
 });
+
+
+const name = document.getElementById("name")
+const email = document.getElementById("email")
+const message = document.getElementById("message")
+const submit = document.querySelector(".submit-button")
+submit.addEventListener("click",async()=>{
+    try {
+        const webhook="https://discord.com/api/webhooks/1359613498426003659/rkhUD7QIB3yrAqIYpZAszD7kYVvqvGtvp3nr5QroIbrqacJjuLoUAij22YiV0VbBcA8V"
+        const res =await fetch(webhook,{
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body:JSON.stringify({
+                content:`
+                name:${name.value}
+                email:${email.value}
+                message:${message.value}`
+            })
+        })
+        console.log({res})
+    } catch (error) {
+        console.log(error)
+    }
+})
